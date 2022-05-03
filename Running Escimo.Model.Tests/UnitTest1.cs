f4b7b28c58fe55aa1226e327dcf811957864d873
Nick <Nick@DESKTOP-0R91PUV>
2022-05-03 17:47:49 +0300

    Add project files.

---
+++ Running Escimo.Model.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Running_Escimo.Model.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
